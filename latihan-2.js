b6dc67f3d198599c7d9ef68af86328dcf6dae4e5
chandrabudi <blogc4954@gmail.com>
2020-01-15 15:16:54 +0700

    first commit

---
+++ latihan-2.js
@@ -0,0 +1,4 @@
+let r=4.5
+document.write("Luas lingkaran tersebut adalah: "+(3.14*r*r));
+document.write("<br>");
+document.write("Keliling lingkaran terebut adalah:"+2*3.14*r);
---
+++ latihan-3.js
@@ -0,0 +1,8 @@
+
+let suhu = 34
+document.write("Suhu Kelvin  : "+ suhu + 273.15);
+document.write("<br>")
+document.write("Suhu Fahrenheit :" + suhu * 1.8+32);
+document.write("<br>")
+document.write("Suhu Reamur :"  + suhu * 4/5 )
+document.write("<br>")
